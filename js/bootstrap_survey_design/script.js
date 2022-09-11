06c7ee1dd68f0d5626b41a83adf7279c8888fb94
piotr <piotr.dominik.sobol@gmail.com>
2022-09-11 14:16:59 +0200

    Bootstrap survey design - add icons

--- js/bootstrap_survey_design/script.js
+++ js/bootstrap_survey_design/script.js
@@ -1,10 +1,11 @@
-menuArray = ["sectionOne", "sectionSecond", "sectionThird", "sectionFourth", "sectionFifth", "sectionSixth"]
-elementArray = ["menu-one", "menu-second", "menu-third", "menu-fourth", "menu-fifth", "menu-sixth"]
+menuArray = ["sectionOne", "sectionSecond", "sectionThird", "sectionFourth", "sectionFifth", "sectionSixth", "sectionSeventh", "sectionEighth", "sectionNinth"]
+elementArray = ["menu-one", "menu-second", "menu-third", "menu-fourth", "menu-fifth", "menu-sixth", "menu-seventh", "menu-eighth", "menu-ninth"]
 
-for(let i=0; i<6; i++) {
+for(let i=0; i<9; i++) {
     const menu = document.getElementById(menuArray[i]);
     menu.addEventListener("mouseenter", event => {
         document.getElementById(elementArray[i]).style.fontWeight = "bold";}, false);
     menu.addEventListener("mouseleave", event => {
         document.getElementById(elementArray[i]).style.fontWeight = "normal";}, false);
     }
+ 
