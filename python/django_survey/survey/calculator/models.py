a0fcc3b085310c419b070d813f8695e5506fb7b7
piotr <piotr.dominik.sobol@gmail.com>
2022-09-19 08:19:49 +0200

    Django survey - object representation in db

--- python/django_survey/survey/calculator/models.py
+++ python/django_survey/survey/calculator/models.py
@@ -1,6 +1,9 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
+
     feature_1 = models.CharField(max_length=100)
     feature_2 = models.CharField(max_length=100)
     feature_3 = models.CharField(max_length=100)
@@ -13,6 +16,12 @@ class Question(models.Model):
     feature_10 = models.CharField(max_length=100)
     feature_11 = models.CharField(max_length=100)
 
+
+    def publishment(self):
+        return self.feature_4 >= timezone.now() - datetime.timedelta(days=1)
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
+
