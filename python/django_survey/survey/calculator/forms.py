1fb6a9cd04cc949f7e7e53492ddd1934b03c9c85
piotr <piotr.dominik.sobol@gmail.com>
2022-09-26 22:13:33 +0200

    Django survey - forms styling with crispy forms (input style)

--- python/django_survey/survey/calculator/forms.py
+++ python/django_survey/survey/calculator/forms.py
@@ -1,6 +1,6 @@
 from dataclasses import fields
 from pyexpat import model
-from django.forms import ModelForm, widgets
+from django.forms import ModelForm, widgets, TextInput, Select, NumberInput, DateInput
 from calculator.models import *
 from calculator.views import *
 
@@ -9,5 +9,103 @@ class CandidateForm(ModelForm):
         model = Candidate
         fields = '__all__'
         
+        widgets = {
+            'feature_1': Select(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }, choices={
+                ("Feature 1", "Feature 1"),
+                ("Feature 2", "Feature 2"),
+                ("Feature 3", "Feature 3")
+            }),
+
+            'feature_2': Select(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }, choices={
+                ("Feature 1", "Feature 1"),
+                ("Feature 2", "Feature 2"),
+                ("Feature 3", "Feature 3")
+            }),
+
+            'feature_3': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_4': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_5': DateInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_6': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_7': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_8': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_9': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_10': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            }),
+
+            'feature_11': TextInput(attrs={
+                'class': 'form-control',
+                'style': 
+                    'background-color: white;'
+                    'border: 0;'
+                    'box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;'  
+            })
+
+        }
         
         
--- python/django_survey/survey/survey/settings.py
+++ python/django_survey/survey/survey/settings.py
@@ -39,10 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'bootstrap5',
-    
+    'crispy_forms'
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
