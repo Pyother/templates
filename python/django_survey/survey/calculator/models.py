fe3e11360fe9c90887b28e6bca27d1ae5769c765
piotr <piotr.dominik.sobol@gmail.com>
2022-09-27 23:20:53 +0200

    Django survey - add optional fields in forms

--- python/django_survey/survey/calculator/models.py
+++ python/django_survey/survey/calculator/models.py
@@ -2,19 +2,20 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Candidate(models.Model):
 
-    feature_1 = models.CharField(max_length=100)
-    feature_2 = models.CharField(max_length=100)
-    feature_3 = models.CharField(max_length=100)
-    feature_4 = models.CharField(max_length=100)
-    feature_5 = models.CharField(max_length=100)
-    feature_6 = models.CharField(max_length=100)
-    feature_7 = models.DateField(default='01/01/2023')
-    feature_8 = models.CharField(max_length=100)
-    feature_9 = models.CharField(max_length=100)
-    feature_10 = models.CharField(max_length=100)
-    feature_11 = models.CharField(max_length=100)
+    feature_1 = models.CharField('Feature 1:', max_length=100)
+    feature_2 = models.CharField('Feature 2:', max_length=100)
+    feature_3 = models.CharField('Feature 3 (optional):', max_length=100, blank=True)
+    feature_4 = models.CharField('Feature 4 (optional):', max_length=100, blank=True)
+    feature_5 = models.CharField('Feature 5 (optional):', max_length=100, blank=True)
+    feature_6 = models.CharField('Feature 6:', max_length=100)
+    feature_7 = models.DateField('Feature 7:', default='01/01/2023')
+    feature_8 = models.CharField('Feature 8:', max_length=100)
+    feature_9 = models.CharField('Feature 9 (optional):', max_length=100, blank=True)
+    feature_10 = models.CharField('Feature 10 (optional):', max_length=100, blank=True)
+    feature_11 = models.CharField('Feature 11 (optional):', max_length=100, blank=True)
 
     array_elements = [feature_1, feature_2, feature_3, feature_4, feature_5, 
     feature_6, feature_7, feature_8, feature_9, feature_10, feature_11]
