1b75d5118c2d3edfc8e1daf2872578698bdd22f8
piotr <piotr.dominik.sobol@gmail.com>
2022-10-24 12:37:34 +0200

    Django surbey - save form values to the list

--- python/django_survey/survey/calculator/views.py
+++ python/django_survey/survey/calculator/views.py
@@ -1,3 +1,4 @@
+from re import X
 from django.shortcuts import render
 from calculator.forms import CandidateForm
 from django.views.generic import TemplateView
@@ -14,21 +15,28 @@ from django.http import HttpResponse
 
 def index(request):
     
+    # Objects and variables:
     form = CandidateForm()
+    array_elements = []
+
     if request.method == "POST":
+
         print(request.POST)
         form = CandidateForm(request.POST)
 
-        print (form['feature_1'].value())
-        print (form.data['feature_1'])
-
+        # Save form values to the list:
         if form.is_valid():
 
-            print (form.cleaned_data['feature_1'])
-            print (form.instance.feature_1)
-
+            for x in form.cleaned_data:
+                array_elements.append(form[x].value())
+                
             form.save()
+        
+    print("ARRAY ELEMENTS")
+    print(array_elements)
 
+    # Create output file:
+    file = PDF(array_elements=array_elements)
     context = {'form': form}
     return render(request, 'main.html', context)
 
