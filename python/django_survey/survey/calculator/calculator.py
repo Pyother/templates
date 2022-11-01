d6d63b41a7d38d58ca85e3495b9b080676989bdf
piotr <piotr.dominik.sobol@gmail.com>
2022-11-02 00:40:09 +0100

    Django survey - compare elements from uploaded excel file with Django form elements

--- python/django_survey/survey/calculator/calculator.py
+++ python/django_survey/survey/calculator/calculator.py
@@ -7,9 +7,13 @@ class Calculator:
     def __init__(self, array_elements):
 
         self.array_elements = array_elements
-        self.system = System.objects.all().values()
-        print(self.system)
-
+        self.system = list(System.objects.all().values())
+        
+        for i in range (len(self.system)):
+            print("Employer: ", self.system[i]["employer"])
+            print("Element: ", self.array_elements[i])
+            print(self.system[i]["employer"]==self.array_elements[i])
+        
     def __str__(self):
         print(f"VALUES: {self.array_elements}")
 
