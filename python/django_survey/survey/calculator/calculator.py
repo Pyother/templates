13b80937993fa921936184f4fd1be706186beb6c
piotr <piotr.dominik.sobol@gmail.com>
2022-11-02 00:12:17 +0100

    Django survey - get objects from database

--- python/django_survey/survey/calculator/calculator.py
+++ python/django_survey/survey/calculator/calculator.py
@@ -1,3 +1,5 @@
+from .models import System
+
 class Calculator:
 
     array_elements = []
@@ -5,5 +7,10 @@ class Calculator:
     def __init__(self, array_elements):
 
         self.array_elements = array_elements
-        print("VALUES:")
-        print(self.array_elements)
+        self.system = System.objects.all().values()
+        print(self.system)
+
+    def __str__(self):
+        print(f"VALUES: {self.array_elements}")
+
+    
--- python/django_survey/survey/calculator/output.py
+++ python/django_survey/survey/calculator/output.py
@@ -8,16 +8,18 @@ class PDF():
     def __init__(self, array_elements):
 
         self.array_elements = array_elements
-        print("OUTPUT")
-        print(self.array_elements)
         
         self.file.add_page()
         self.file.set_font("Arial", "", 10)
         self.file.multi_cell(w=190, h=10, txt="New page created")
+        
         for i in range(len(array_elements)):
-                self.file.multi_cell(w=190, h=10, txt=array_elements[i])
+            self.file.multi_cell(w=190, h=10, txt=array_elements[i])
 
         self.file.output('calculator\output\Results.pdf')
+
+    def __str__(self):
+        print(f"OUTPUT: {self.array_elements}")
 
 
 
