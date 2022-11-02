75f0bf30d0b9a1a8a54313cb2158d58ccbae3859
piotr <piotr.dominik.sobol@gmail.com>
2022-11-02 11:43:57 +0100

    Django survey - set calculate function

--- python/django_survey/survey/calculator/calculator.py
+++ python/django_survey/survey/calculator/calculator.py
@@ -9,12 +9,36 @@ class Calculator:
 
         self.array_elements = array_elements
         self.system = list(System.objects.all().values())
-        
-        date1 = datetime.datetime.strptime(self.array_elements[6], '%d/%m/%Y')
-        date2 = datetime.datetime.strptime(self.system[0]["border_date"], '%d/%m/%Y')
-        print(date1 == date2)
+        self.candidate_system = self.calculate(self.array_elements, self.system)
+
+        print(self.candidate_system)
+
         
     def __str__(self):
         print(f"VALUES: {self.array_elements}")
+
+    def dates_comparison(self, str1, str2):
+
+        return (datetime.datetime.strptime(str1, '%d/%m/%Y') <= 
+            datetime.datetime.strptime(str2, '%d/%m/%Y'))
+
+    def calculate(self, array_elements, system):
+
+        candidate_system = []
+
+        for i in range(len(system)):
+            if ((array_elements[2] == system[i]["employer"]) and
+                (array_elements[3] == system[i]["substract"]) and 
+                (self.dates_comparison(array_elements[6], system[i]["border_date"]) == True )):
+                
+                values = {"substract": system[i]["substract"],
+                          "value": system[i]["value"],
+                          "comment": system[i]["comment"]}
+                candidate_system.append(values)
+                
+            else:
+                print("Not matches")
+            
+        return candidate_system
 
     
