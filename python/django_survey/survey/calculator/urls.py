15b84ae443dedee15ac73f77021143d60eb86da6
piotr <piotr.dominik.sobol@gmail.com>
2022-11-14 15:16:45 +0100

    Django surbey - redirection from main to the output page

--- python/django_survey/survey/calculator/urls.py
+++ python/django_survey/survey/calculator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from calculator.views import index
 from .views import *
 
-urlpatterns = [
+urlpatterns = [ 
     path('', index, name='index'),
     path('output/', redirect_output, name='output')
 ] 
